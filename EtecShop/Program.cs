ed5d0ac398ac93a9de1a318fd2ca95ab52792e48
JoseMaksud <jf9718830@gmail.com>
2024-06-04 20:26:14 -0300

    Criado a parte de register e login

--- EtecShop/Program.cs
+++ EtecShop/Program.cs
@@ -1,18 +1,30 @@
 using EtecShop.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 string conn = builder.Configuration.GetConnectionString("EtecShopConnectionString");
 builder.Services.AddDbContext<AppDbContext>(
 opt => opt.UseInMemoryDatabase(conn)
 );
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(
+    opt => opt.SignIn.RequireConfirmedAccount = false
+)
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
 var app = builder.Build();
+
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     context.Database.EnsureCreated();
 }
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -21,11 +33,15 @@ if (!app.Environment.IsDevelopment())
     // scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
+
 app.MapControllerRoute(
 name: "default",
 pattern: "{controller=Home}/{action=Index}/{id?}");
